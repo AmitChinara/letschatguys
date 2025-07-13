bf84ce210cceb9cf7c69fb59bb38058188c97ed2
Amit Chinara <amitchinara@gmail.com>
2025-07-13 16:13:57 +0530

    Front: connection testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import socket from "./socket";
 import LoginPage from "./pages/LoginPage";
 import ChatPage from "./pages/ChatPage";
 import { UserProvider } from "./context/UserContext";
-import "./App.css";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return (
-      <UserProvider>
-        {isLoggedIn ? <ChatPage /> : <LoginPage onLogin={() => setIsLoggedIn(true)} />}
-      </UserProvider>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [socketReady, setSocketReady] = useState(false);
+
+    useEffect(() => {
+        socket.connect();
+
+        socket.on("connect", () => {
+            console.log("Connected to server");
+            setSocketReady(true);
+        });
+
+        socket.on("connect_error", (err) => {
+            console.log("Socket connect error", err);
+            setSocketReady(false);
+        });
+
+        return () => {
+            socket.off("connect");
+            socket.off("connect_error");
+        };
+    }, []);
+
+    if (!socketReady) {
+        return <div style={{ textAlign: "center", marginTop: "30vh" }}>🔄 Connecting to server...</div>;
+    }
+
+    return (
+        <UserProvider>
+            {isLoggedIn ? <ChatPage /> : <LoginPage onLogin={() => setIsLoggedIn(true)} />}
+        </UserProvider>
+    );
 }
 
 export default App;
--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,8 @@
+// src/socket.js
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000"); // replace with your backend URL
+const socket = io("https://letschatguys-backend.onrender.com", {
+    autoConnect: false,
+});
+
 export default socket;
